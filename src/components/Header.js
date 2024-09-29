fec13a3832cd9a098d301f86df3622e1f36f7351
Davi Micale <62074503+davimicale@users.noreply.github.com>
2024-09-29 20:56:43 -0300

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,1 +1,18 @@
+import React from 'react';
+import './styles.css';
 
+const Header = () => {
+  return (
+    <header>
+      <div className="logo">Patinho Feio do Legume</div>
+      <input type="text" placeholder="Buscar..." className="search-bar" />
+      <nav className="nav-links">
+        <a href="#"><i className="fas fa-box"></i> Meus pedidos</a>
+        <a href="#"><i className="fas fa-user"></i> Minha conta</a>
+        <a href="#"><i className="fas fa-shopping-cart"></i> Carrinho de compras</a>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
